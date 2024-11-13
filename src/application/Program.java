755208f162a9ce34f8fac020c2d5f551122f7e7b
LuizantonioDev9 <luizantoniodev9@gmail.com>
2024-11-13 14:31:02 -0300

    finalizando a classe product

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
+import model.entities.Product;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
@@ -12,7 +15,7 @@ public class Program {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Locale.setDefault(Locale.US);
-        List<Product>
+        List<Product> list = new ArrayList<>();
 
         System.out.println("Entre com o caminho do arquivo: ");
         //colocar o caminho do arquivo ex: c:/temp/arquivo.csv
--- src/model/entities/Product.java
+++ src/model/entities/Product.java
@@ -1,4 +1,49 @@
 package model.entities;
 
 public class Product {
+    //atr
+    private String name;
+    private double price;
+    private int quantity;
+
+    //constr
+    public Product() {
+
+    }
+
+    public Product(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    //getters e setters
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    //metodo
+    public double total() {
+        double sum = 0.0;
+        sum += quantity * price;
+        return sum;
+    }
 }
